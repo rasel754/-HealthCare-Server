cbab176c0709adc9ffefd155644efb6d17085eb4
MD Rasel Ahmed <raselahmed73614@gmail.com>
2026-08-04 15:10:56 +0600

    better auth setup

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { SpecialtyRouter } from "../module/specialty/specialty.route";
+import { authRouters } from "../module/auth/auth.route";
 
 const router =Router();
 
+router.use('/auth',authRouters);
 router.use('/specialty',SpecialtyRouter);
 
 
