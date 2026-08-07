5bc24fcdf96b66c659de65ac5dcff3c054eb2a7b
MD Rasel Ahmed <raselahmed73614@gmail.com>
2026-08-07 12:59:19 +0600

    introduce zod in my project

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,12 +1,63 @@
-import { NextFunction, Request, Response } from "express"
-import httpStatus from "http-status"
+import { NextFunction, Request, Response } from "express";
+import status from "http-status";
+import z from "zod";
+import { TErrorResponse, TErrorSources } from "../interface/error.interface";
+import { envVars } from "../../config/env";
+import { handleZodError } from "../errorHelpers/handleZodError";
+import AppError from "../errorHelpers/AppError";
 
-const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+
+
+export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    if (envVars.NODE_ENV === 'development') {
+        console.log("Error from Global Error Handler", err);
+    }
+
+    let errorSources: TErrorSources[] = []
+    let statusCode: number = status.INTERNAL_SERVER_ERROR;
+    let message: string = 'Internal Server Error';
+    let stack: string | undefined = undefined;
+
+    if (err instanceof z.ZodError) {
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError.statusCode as number
+        message = simplifiedError.message
+        errorSources = [...simplifiedError.errorSources]
+        stack = err.stack;
+
+    } else if (err instanceof AppError) {
+        statusCode = err.statusCode;
+        message = err.message;
+        stack = err.stack;
+        errorSources = [
+            {
+                path: '',
+                message: err.message
+            }
+        ]
+    }
+    else if (err instanceof Error) {
+        statusCode = status.INTERNAL_SERVER_ERROR;
+        message = err.message
+        stack = err.stack;
+        errorSources = [
+            {
+                path: '',
+                message: err.message
+            }
+        ]
+    }
+
+
+    const errorResponse: TErrorResponse = {
         success: false,
-        message: err.name || "Something went wrong!",
-        error: err
-    })
-};
+        message: message,
+        errorSources,
+        error: envVars.NODE_ENV === 'development' ? err : undefined,
+        stack: envVars.NODE_ENV === 'development' ? stack : undefined,
+    }
+
+    res.status(statusCode).json(errorResponse);
+}
 
 export default globalErrorHandler;
--- src/app/module/user/user.route.ts
+++ src/app/module/user/user.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { UserControllers } from "./user.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { createDoctorZodSchema } from "./user.validation";
 
 const router =Router();
 
-router.post("/create-doctor", UserControllers.createDoctor);
+router.post("/create-doctor",validateRequest(createDoctorZodSchema), UserControllers.createDoctor);
 
 export const UserRoutes = router;
