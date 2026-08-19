a336597640e4c9113f784e992d1ca67e64751f51
rasel754 <raselahmed73614@gmail.com>
2026-08-19 23:46:43 +0600

    cloudinary file delete function implimented

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -1,3 +1,5 @@
+import status from "http-status";
+import AppError from "../app/errorHelpers/AppError";
 import { envVars } from "./env";
 import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 
@@ -6,4 +8,30 @@ cloudinary.config({
     api_key: envVars.CLOUDINARY.CLOUDINARY_API_KEY,
     api_secret: envVars.CLOUDINARY.CLOUDINARY_API_SECRET,
 })
+
+export const deleteFileFromCloudinary = async (url : string) => {
+
+    try {
+        const regex = /\/v\d+\/(.+?)(?:\.[a-zA-Z0-9]+)+$/;
+
+        const match = url.match(regex);
+
+        if (match && match[1]) {
+            const publicId = match[1];
+
+            await cloudinary.uploader.destroy(
+                publicId, {
+                resource_type: "image"
+            }
+            )
+
+            console.log(`File ${publicId} deleted from cloudinary`);
+        }
+
+    } catch (error) {
+        console.error("Error deleting file from Cloudinary:", error);
+        throw new AppError(status.INTERNAL_SERVER_ERROR, "Failed to delete file from Cloudinary");
+    }
+}
+
 export const cloudinaryUpload = cloudinary;
