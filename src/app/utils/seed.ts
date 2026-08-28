7680df0fc6e2bd025e7994fb2d8292a396c75197
rasel754 <raselahmed73614@gmail.com>
2026-08-28 15:04:08 +0600

    refactor sever cdoe

--- src/app/utils/seed.ts
+++ src/app/utils/seed.ts
@@ -6,55 +6,55 @@ import { prisma } from "../lib/prisma";
 export const seedSuperAdmin = async () => {
     try {
         const isSuperAdminExist = await prisma.user.findFirst({
-            where:{
-                role : Role.SUPER_ADMIN
+            where: {
+                role: Role.SUPER_ADMIN
             }
         })
 
-        if(isSuperAdminExist) {
+        if (isSuperAdminExist) {
             console.log("Super admin already exists. Skipping seeding super admin.");
             return;
         }
 
         const superAdminUser = await auth.api.signUpEmail({
-            body:{
-                email : envVars.SUPER_ADMIN_EMAIL,
-                password : envVars.SUPER_ADMIN_PASSWORD,
-                name : "Super Admin",
-                role : Role.SUPER_ADMIN,
-                needPasswordChange : false,
-                rememberMe : false,
+            body: {
+                email: envVars.SUPER_ADMIN_EMAIL,
+                password: envVars.SUPER_ADMIN_PASSWORD,
+                name: "Super Admin",
+                role: Role.SUPER_ADMIN,
+                needPasswordChange: false,
+                rememberMe: false,
             }
         })
 
         await prisma.$transaction(async (tx) => {
             await tx.user.update({
-                where : {
-                    id : superAdminUser.user.id
+                where: {
+                    id: superAdminUser.user.id
                 },
-                data : {
-                    emailVerified : true,
+                data: {
+                    emailVerified: true,
                 }
             });
 
             await tx.admin.create({
-                data : {
-                    userId : superAdminUser.user.id,
-                    name : "Super Admin",
-                    email : envVars.SUPER_ADMIN_EMAIL,
+                data: {
+                    userId: superAdminUser.user.id,
+                    name: "Super Admin",
+                    email: envVars.SUPER_ADMIN_EMAIL,
                 }
             })
 
-            
-            
+
+
         });
 
         const superAdmin = await prisma.admin.findFirst({
-            where : {
-                email : envVars.SUPER_ADMIN_EMAIL,
+            where: {
+                email: envVars.SUPER_ADMIN_EMAIL,
             },
-            include : {
-                user : true,
+            include: {
+                user: true,
             }
         })
 
@@ -62,8 +62,8 @@ export const seedSuperAdmin = async () => {
     } catch (error) {
         console.error("Error seeding super admin: ", error);
         await prisma.user.delete({
-            where : {
-                email : envVars.SUPER_ADMIN_EMAIL,
+            where: {
+                email: envVars.SUPER_ADMIN_EMAIL,
             }
         })
     }
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,76 @@
+import { Server } from "http";
 import app from "./app";
 import { seedSuperAdmin } from "./app/utils/seed";
 import { envVars } from "./config/env";
 
-
-
-
-const bootstrap = async () => {
+let server : Server;
+const bootstrap = async() => {
     try {
         await seedSuperAdmin();
-        app.listen(envVars.PORT || 5000, () => {
-            console.log( `Server is running on port ${envVars.PORT || 5000}`);
+        server = app.listen(envVars.PORT, () => {
+            console.log(`Server is running on http://localhost:${envVars.PORT}`);
         });
     } catch (error) {
-        console.error("Error starting the server:", error);
+        console.error('Failed to start server:', error);
     }   
 }
+
+// SIGTERM signal handler
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal received. Shutting down server...");
+
+    if(server){
+        server.close(() => {
+            console.log("Server closed gracefully.");
+            process.exit(1);
+        });
+    } 
+    
+    process.exit(1);
+    
+})
+
+// SIGINT signal handler
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal received. Shutting down server...");
+
+    if(server){
+        server.close(() => {
+            console.log("Server closed gracefully.");
+            process.exit(1);
+        });
+
+    }
+
+    process.exit(1);
+});
+
+//uncaught exception handler
+process.on('uncaughtException', (error) => {
+    console.log("Uncaught Exception Detected... Shutting down server", error);
+
+    if(server){
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+
+    process.exit(1);
+})
+
+process.on("unhandledRejection", (error) => {
+    console.log("Unhandled Rejection Detected... Shutting down server", error);
+
+    if(server){
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+
+    process.exit(1);
+})
+
+//unhandled rejection handler
 
 bootstrap();
